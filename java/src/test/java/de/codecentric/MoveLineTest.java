84afd2b299aaf92cbcf9ad455d2b6df493259f27
Thomas Traude <f-thomas.traude@continentale.de>
2025-07-02 10:53:31 +0200

    Extend MovelLine instructions for Eclipse users, reformat code

--- java/src/test/java/de/codecentric/MoveLineTest.java
+++ java/src/test/java/de/codecentric/MoveLineTest.java
@@ -9,19 +9,29 @@ import org.junit.jupiter.api.Test;
  * <h1>Move Line(s)</h1>
  *
  * Run the test {@link #number_strings_are_in_their_numerical_order()} and see it failing.
- * <p>
- * Fix the test by editing the {@link #getNumbers()} method:
- * For this, sort the lines by using only keyboard shortcuts for
+ *
+ * <p>Fix the test by editing the {@link #getNumbers()} method: For this, sort the lines by using
+ * keyboard shortcuts for the following commands:
+ *
+ * <h2>IntelliJ</h2>
+ *
  * <ul>
- *  <li>move statment up</li>
- *  <li>move statment down</li>
+ *   <li>move statment up
+ *   <li>move statment down
  * </ul>
- * <p>
- * Hint: You do not need to select text for moving a line.
- * Instead, place the cursor on the line you want to move.
- * You can move multiple lines at a time by extending the selection.
- * <p>
- * Also see <a href="https://www.jetbrains.com/guide/java/tips/move-line">Jetbrains Guide - Move Line</a>
+ *
+ * <h2>Eclipse</h2>
+ *
+ * <ul>
+ *   <li>move lines up
+ *   <li>move lines down
+ * </ul>
+ *
+ * <p>Hint: You do not need to select text for moving a line. Instead, place the cursor on the line
+ * you want to move. You can move multiple lines at a time by extending the selection.
+ *
+ * <p>Also see <a href="https://www.jetbrains.com/guide/java/tips/move-line">Jetbrains Guide - Move
+ * Line</a>
  */
 class MoveLineTest implements WithAssertions {
 
@@ -29,18 +39,9 @@ class MoveLineTest implements WithAssertions {
   void number_strings_are_in_their_numerical_order() {
     List<String> numbers = getNumbers();
 
-    assertThat(numbers).containsExactly(
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "ten"
-    );
+    assertThat(numbers)
+        .containsExactly(
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
   }
 
   private List<String> getNumbers() {
